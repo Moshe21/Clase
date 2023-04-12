94b7ef85f057bb13134f603abbd84bae0a91c3ca
Moshe <83256516+Moshe21@users.noreply.github.com>
2023-04-11 20:29:57 -0500

    media clase

---
+++ hello world.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (){
+	
+	int num1,num2,num3;
+	int i=0;
+		
+	printf("ingrese su numero");
+		
+		
+	scanf("%d\n",num1);
+	printf("%d\n",num1);
+	printf("ingrese su numero");
+	scanf("%d\n",num2);
+	printf("%d\n",num2);
+	printf("ingrese su numero");
+	
+	scanf("%d\n",num3);
+	printf("%d\n",num3);
+
+	
+	
+}
+
+ 
