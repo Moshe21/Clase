d5f895fa7eaedfa001d582dc8636b986734bf199
Moshe <83256516+Moshe21@users.noreply.github.com>
2023-04-25 20:56:47 -0500

    class 25 casita azul

--- programacion/hello world.cpp
+++ programacion/hello world.cpp
@@ -2,12 +2,38 @@
 
 int main (){
 	
+	while(1){
+		int opcion;
+		printf("BIENVENIDOS AL COLEGIO \n      CASITA AZUL\n 1.INGRESAR\n 2.REGISTRASE\n 3.EXIT\n ingrese una opcion\t");
+		scanf("%d",opcion);
+		
+		switch(opcion){
+	
+		
+			case 1:{
+					menu_ing();
+					break;
+				}
+			case 2:{
+					menu_reg();
+					break;
+				}
+			case 3:{
+					exit(1);
+				}
+			default:
+				printf("bi bu bi bu error vueleve a intentar");
+		
+		}	
+	}			
+}
 
-		
-	printf("ingrese su sadasdasd   asafdasd  numerxcvsdfgv");
-		
-	
-	
+menu_ing(){
+	int opcion
+	printf("INGRESAR \n Usuario");
+	scanf("%d", opcion);
+	printf("\n contraseña");
+	scanf("%d", opcion);
 	
 }
 
