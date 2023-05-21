f2d612ea07d1a23b65cbaef904cc8d5df5cbf104
Moshe <83256516+Moshe21@users.noreply.github.com>
2023-05-21 11:10:07 -0500

    taller 8 casi terminado
    vamos genial

--- SQL SERVER/taller 8(compras).sql
+++ SQL SERVER/taller 8(compras).sql
@@ -1,28 +1,77 @@
+--=================================================================================================================================== 
+--PRACTICA 8 DE LENGUAJE ESTRUCTURADO SQL ("Educando S.A")
+--MOISES ZABALETA CRUZ
+--1033810224
+--Email: MZABALETAC@ACADEMIA.USBBOG.EDU.CO
+--================================================================================================================================ 
+
 create database EducandoBD;
 use EducandoBD;
 
-
-create table solicitud(
-N_solicitud varchar (4),
-fecha date,
-nombre varchar(40)
-apellido varchar (40),
-C_costos varchar(4)
-C_presupuesto int,
-primary key (N_solicitud)
-);
-
-create table item(
-Item varchar (4),
-N_producto varchar(40),
+create table Item(
+N_item varchar (4),
+Nom_producto varchar(40),
 cantidad int,
 U_medida varchar (40),
-valor_U int,
-valor_T int,
+V_unidad int,
+V_total int,
 
-primary key (Item)
+primary key (N_item)
 );
 
 create table proveedor(
+N_pedido varchar(10),
 nit varchar(10),
-nombre_p varchar(20),
+Nom_proveedor varchar(20),
+F_orden date,
+T_orden int,
+F_entrega date,
+N_item varchar (4),
+primary key (n_pedido)
+);
+
+-- aprobada por el Director Financiero
+
+create table factura(
+N_factura varchar(10),
+N_Entrada varchar(10),
+F_factura date,
+N_pedido varchar(10),
+N_item varchar (4),
+primary Key (N_factura)
+);
+
+
+create table solicitud(
+N_solicitud varchar (10),
+T_solicitud int,
+F_solicitud date,
+nombre varchar(40),
+apellido varchar (40),
+cargo varchar(60),
+C_costos int,
+C_presupuesto int,
+T_salida int,
+F_salida date,
+T_entrega int,
+F_entrega date,
+direccion varchar(60),
+
+--proveedor foranea
+-- item foranea
+
+primary key (N_solicitud)
+);
+
+alter table proveedor	
+	add constraint fk_proveedor_item
+	foreign key (N_item) 
+	references  item(N_item);
+
+alter table factura	
+	add constraint fk_factura_item
+	foreign key (N_item) 
+	references  item(N_item);
+
+
+	
