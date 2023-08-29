75a367f585dd9c7eae8a775453958f4e6dbee8d4
Moshe <83256516+Moshe21@users.noreply.github.com>
2023-08-28 20:39:50 -0500

    formulario

---
+++ formulario/src/formulario/Formulario.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formulario;
+
+/**
+ *
+ * @author 304
+ */
+public class Formulario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        frmcaptura primer_formulario=new frmcaptura();
+        primer_formulario.setVisible(true);
+                
+// TODO code application logic here
+    }
+    
+}
