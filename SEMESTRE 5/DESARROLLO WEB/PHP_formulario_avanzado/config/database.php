658f4b37e10cacf7596259a49dc9ed6aa0933e78
Moshe <83256516+Moshe21@users.noreply.github.com>
2024-09-12 19:32:54 -0500

    database php

---
+++ SEMESTRE 5/DESARROLLO WEB/PHP_formulario_avanzado/config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+function connectarDB():mysqli{
+
+    $DB= mysqli_connect('localhost','root','091159Eliza*',
+    'pqrs230831');
+
+    if(!$db){
+
+        echo ">Error: No se pudo conectar";
+        exit;
+    }
+
+
+}
+
+?>
