ac9526df582c3cef58670a2e8c8c605ef32de3c3
Moshe <83256516+Moshe21@users.noreply.github.com>
2024-09-17 18:31:22 -0500

    clase de data minng

--- SEMESTRE 5/DESARROLLO WEB/Formulario PQRS/conexion.php
+++ SEMESTRE 5/DESARROLLO WEB/Formulario PQRS/conexion.php
@@ -1,12 +1,11 @@
 <?php
 
 // declaracion de variables
-$sever="";
-$user="freedb_ADMIN_MOSHE2";
-$pass="3Br$%RtKc8dYcE5";
+$sever="sql.freedb.tech";
+$user="freedb_ADMIN_MOSHE3";
+$pass="#!zPKyTGv?6GjdT";
 $dbname="freedb_My_server";
 $db_port="3306";
-
 
 $conx1=mysqli_connect($sever,$user,$pass,$dbname,$db_port);
 
